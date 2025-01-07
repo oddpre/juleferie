f142020783d431dd0bc7d722dc14a206dcbf9d82
Odd Adrian Prestegård <131375320+oddpre@users.noreply.github.com>
2025-01-07 16:34:26 +0100

    4pm changes
    Meets the criteria of the task.
    
    Until a new task is available; try to make a better dialogue box as shown in the example. For now it's just an alertbox, but it still serves the same purpose.

--- scripts.js
+++ scripts.js
@@ -6,3 +6,20 @@ const menuDropDownMobile = () =>
 }
 
 
+//Validation of the form and summmoning of a "Thank you for your message" message. 
+
+const validateForm = () =>
+{
+    //Can already validate email with the type="email" attribute. If checking for email through JS, use of regex is the way to go. 
+    /* Example below.
+    function validateEmail(email) 
+    {
+        var re = /\S+@\S+\.\S+/;
+        return re.test(email);
+    }
+
+
+    */
+    alert("Thank you for your message!");
+
+}
